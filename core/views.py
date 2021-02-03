8df6d8ac52300faafa649d74cfb28982ec9057ed
Saksham Mittal <saksham1991999@users.noreply.github.com>
2021-02-03 11:28:32 +0530

    custom delete

--- core/views.py
+++ core/views.py
@@ -1,11 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 from core.models import Location
 from core.serializers import LocationSerializer
 from rest_framework import viewsets, status
 from rest_framework.response import Response
-
+from rest_framework.decorators import action
 # ViewSets define the view behavior.
 class LocationViewSet(viewsets.ModelViewSet):
     queryset = Location.objects.all()
@@ -31,3 +31,21 @@ class LocationViewSet(viewsets.ModelViewSet):
             return Response("Removed", status=status.HTTP_204_NO_CONTENT)
         except:
             return Response("Does not exist", status=status.HTTP_204_NO_CONTENT)
+
+    @action(detail=False)
+    def delete_custom(self, request):
+        serializer_context = {
+            'request': request,
+        }
+        try:
+            lat = request.data['lat']
+            long = request.data['long']
+            try:
+                location = get_object_or_404(Location, lat=lat, long=long)
+                location.delete()
+                return Response("Deleted", status=status.HTTP_204_NO_CONTENT)
+            except:
+                return Response("Does not exist", status=status.HTTP_404_NOT_FOUND)
+
+        except:
+            return Response("Data is not sufficient", status=status.HTTP_400_BAD_REQUEST)
